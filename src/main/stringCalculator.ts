b4c1413b5236eca6d02ce6fef2b77dcccc96706a
Mark Said Camilleri <mark.saidcamilleri@cazoo.co.uk>
2021-11-11 11:19:26 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/stringCalculator.ts
+++ src/main/stringCalculator.ts
@@ -1,14 +1,8 @@
 export class StringCalculator {
     add(input: string) {
+        if (input === "") return 0;
+
         const inputNumbers = input.split(",");
-
-        if(inputNumbers[1]){
-            return parseInt(inputNumbers[0]) + parseInt(inputNumbers[1])
-        }
-
-        if (input !== "") {
-            return parseInt(input);
-        }
-        return 0;
+        return inputNumbers.reduce((total, num) => parseInt(num) + total, 0)
     }
 }
--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -4,14 +4,17 @@ let calculator: StringCalculator = new StringCalculator();
 
 describe('A StringCalculator Add function', () => {
     it.each`
-    input     | expectedOutput
-    ${""}     | ${0}
-    ${"4"}    | ${4}
-    ${"5"}    | ${5}
-    ${"2,3"}  | ${5}
-    ${"1,5"}  | ${6}
-    ${"10,11"}| ${21}
-    ${"3,5,9"}| ${17}
+    input                | expectedOutput
+    ${""}                | ${0}
+    ${"1"}               | ${1}
+    ${"0,1"}             | ${1}
+    ${"2"}               | ${2}
+    ${"1,2"}             | ${3}
+    ${"2,3"}             | ${5}
+    ${"3,4,1"}           | ${8}
+    ${"10,3"}            | ${13}
+    ${"10,5,5,1"}        | ${21}
+    ${"10,10,5\n5,1\n3"} | ${34}
     `("should return $expectedOutput if it receives $input", ({input, expectedOutput}) => {
         expect(calculator.add(input)).toEqual(expectedOutput);
     })
